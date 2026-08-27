9c2594704501eb4648c50dcf7f9ea0834bb0926f
JUAN ANDRÉS SOSA GARCIA <andressosa1238@gmail.com>
2026-08-27 07:49:48 -0300

    chore: estructura inicial del proyecto , conexion BD y CSS

---
+++ login/config/database.php
@@ -0,0 +1,11 @@
+<?php
+$host = 'localhost';
+$dbname = 'login_system';
+$username = 'root';
+$password = '';
+try {
+$pdo = new PDO("mysql:host=$host;dbname=$dbname", $username , $password);
+$pdo->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+}
+?>
